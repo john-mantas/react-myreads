5009d2c44ca639e660e80f42eedb2d098d73ae39
John Mantas <mantasjohn67@gmail.com>
2018-08-09 01:12:19 +0300

    refactor: refactor html to react components

---
+++ src/Bookshelf.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Book from './Book'
+
+class Bookshelf extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.shelf.map((books) => (<li key={books.id}><Book book={books}/></li>))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Bookshelf;
