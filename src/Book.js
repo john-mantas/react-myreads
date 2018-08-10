1937b7ffb171f5df70db0485c889dc6103c6cd26
John Mantas <mantasjohn67@gmail.com>
2018-08-11 01:20:44 +0300

    fix: Add fallback if book properties not available

--- src/Book.js
+++ src/Book.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 class Book extends React.Component {
   render() {
+    let image, title, authors
+    this.props.book.imageLinks ? image = this.props.book.imageLinks.smallThumbnail : image = ''
+    this.props.book.title ? title = this.props.book.title : title = 'No title available'
+    this.props.book.authors ? authors = this.props.book.authors : authors = 'No authors available'
+
     return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.smallThumbnail})` }}></div>
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="move" disabled>Move to...</option>
@@ -16,8 +21,8 @@ class Book extends React.Component {
             </select>
           </div>
       </div>
-      <div className="book-title">{this.props.book.title}</div>
-      <div className="book-authors">{this.props.book.authors}</div>
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{authors}</div>
     </div>
     );
   }
